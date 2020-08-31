08e02c319bf8b797d0a04b0861e9f23f3b6c8d4b
UberGeoff <geoffrey.leroux@standardbank.co.za>
2020-09-01 00:16:12 +0200

    Update layout

--- apps/flingo/src/app/layouts/full/full.component.ts
+++ apps/flingo/src/app/layouts/full/full.component.ts
@@ -1,65 +1,55 @@
-
 import { MediaMatcher } from '@angular/cdk/layout';
 import { Router } from '@angular/router';
-import {
-  ChangeDetectorRef,
-  Component,
-  OnDestroy
-
-
-} from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
 import { MenuItems } from '../../shared/menu-items/menu-items';
-
 
 import { PerfectScrollbarConfigInterface, PerfectScrollbarDirective } from 'ngx-perfect-scrollbar';
 
 /** @title Responsive sidenav */
 @Component({
-  selector: 'app-full-layout',
-  templateUrl: 'full.component.html',
-  styleUrls: []
+    selector: 'app-full-layout',
+    templateUrl: 'full.component.html',
+    styleUrls: []
 })
 export class FullComponent implements OnDestroy {
-  mobileQuery: MediaQueryList;
-  dir = 'ltr';
-  green = false;
-  blue = false;
-  dark = false;
-  minisidebar = false;
-  boxed = false;
-  danger = false;
-  showHide = false;
-  url = '';
-  sidebarOpened = false;
-  status = false;
+    mobileQuery: MediaQueryList;
+    dir = 'ltr';
+    green = false;
+    blue = false;
+    dark = false;
+    minisidebar = false;
+    boxed = false;
+    danger = false;
+    showHide = false;
+    url = '';
+    sidebarOpened = false;
+    status = false;
 
-  public showSearch = false;
+    public showSearch = false;
 
-  public config: PerfectScrollbarConfigInterface = {};
-  private _mobileQueryListener: () => void;
+    public config: PerfectScrollbarConfigInterface = {};
+    private _mobileQueryListener: () => void;
 
-  clickEvent() {
-    this.status = !this.status;
-  }
+    clickEvent() {
+        this.status = !this.status;
+    }
 
+    constructor(
+        public router: Router,
+        changeDetectorRef: ChangeDetectorRef,
+        media: MediaMatcher,
+        public menuItems: MenuItems
+    ) {
+        this.mobileQuery = media.matchMedia('(min-width: 920px)');
+        this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+        // tslint:disable-next-line: deprecation
+        this.mobileQuery.addListener(this._mobileQueryListener);
+    }
 
-  constructor(
-    public router: Router,
-    changeDetectorRef: ChangeDetectorRef,
-    media: MediaMatcher,
-    public menuItems: MenuItems
-  ) {
-    this.mobileQuery = media.matchMedia('(min-width: 768px)');
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    // tslint:disable-next-line: deprecation
-    this.mobileQuery.addListener(this._mobileQueryListener);
-  }
+    ngOnDestroy(): void {
+        // tslint:disable-next-line: deprecation
+        this.mobileQuery.removeListener(this._mobileQueryListener);
+    }
 
-  ngOnDestroy(): void {
-    // tslint:disable-next-line: deprecation
-    this.mobileQuery.removeListener(this._mobileQueryListener);
-  }
-
-
-  // Mini sidebar
+    // Mini sidebar
 }
