b84a48dc0f52556f051f02f86c1e6e87605b3826
UberGeoff <geoffrey.leroux@standardbank.co.za>
2021-03-17 21:31:43 +0200

    Fix tests

--- libs/workspace/grid/src/lib/components/controls/checkbox/checkbox.component.spec.ts
+++ libs/workspace/grid/src/lib/components/controls/checkbox/checkbox.component.spec.ts
@@ -1,19 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CheckboxComponent } from './checkbox.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
-import { ActionButtonComponent } from './action-button.component';
-
-describe('NewButtonComponent', () => {
-    let component: ActionButtonComponent;
-    let fixture: ComponentFixture<ActionButtonComponent>;
+describe('CheckboxComponent', () => {
+    let component: CheckboxComponent;
+    let fixture: ComponentFixture<CheckboxComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [ActionButtonComponent]
+            imports: [MatCheckboxModule],
+            declarations: [CheckboxComponent]
         }).compileComponents();
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(ActionButtonComponent);
+        fixture = TestBed.createComponent(CheckboxComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
