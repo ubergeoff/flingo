949fba2b851d405486a7c8c0929e58bd9e0a2e4d
UberGeoff <geoffrey.leroux@standardbank.co.za>
2021-03-18 10:49:26 +0200

    Update tests

--- libs/workspace/slick/src/lib/components/infinite-center-slick/infinite-center-slick.component.spec.ts
+++ libs/workspace/slick/src/lib/components/infinite-center-slick/infinite-center-slick.component.spec.ts
@@ -5,21 +5,21 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { CenterSlickComponent } from './center-slick.component';
+import { InfiniteCenterSlickComponent } from './infinite-center-slick.component';
 
-describe('CenterSlickComponent', () => {
-    let component: CenterSlickComponent;
-    let fixture: ComponentFixture<CenterSlickComponent>;
+describe('InfiniteCenterSlickComponent', () => {
+    let component: InfiniteCenterSlickComponent;
+    let fixture: ComponentFixture<InfiniteCenterSlickComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [SlickModule, MatButtonModule, MatCardModule, MatIconModule, MatMenuModule, MatIconModule],
-            declarations: [CenterSlickComponent]
+            declarations: [InfiniteCenterSlickComponent]
         }).compileComponents();
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(CenterSlickComponent);
+        fixture = TestBed.createComponent(InfiniteCenterSlickComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
