7dcf47c4a0a703526a62716760cfd814d76d65e0
Geoffrey Le Roux <geoffrey.leroux@standardbank.co.za>
2022-08-30 23:48:49 +0200

    Updates

--- libs/buttons/src/lib/components/tick-button/tick-button.component.spec.ts
+++ libs/buttons/src/lib/components/tick-button/tick-button.component.spec.ts
@@ -1,25 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TickButtonComponent } from './tick-button.component';
+import { MatIconModule } from '@angular/material/icon';
 
 describe('TickButtonComponent', () => {
-  let component: TickButtonComponent;
-  let fixture: ComponentFixture<TickButtonComponent>;
+    let component: TickButtonComponent;
+    let fixture: ComponentFixture<TickButtonComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ TickButtonComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(
+        waitForAsync(() => {
+            TestBed.configureTestingModule({
+                imports: [MatIconModule],
+                declarations: [TickButtonComponent]
+            }).compileComponents();
+        })
+    );
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TickButtonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TickButtonComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
