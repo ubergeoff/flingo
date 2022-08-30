8868d030c20c675ac70d9ba10cde001c512293e6
Geoffrey Le Roux <geoffrey.leroux@standardbank.co.za>
2022-08-30 20:28:05 +0200

    Updates

--- libs/spinner/src/index.ts
+++ libs/spinner/src/index.ts
@@ -1,1 +1,4 @@
+export { BubbleSpinnerComponent } from './lib/components/bubble/bubble.component';
+export { SpinnerComponent } from './lib/components/spinner/spinner.component';
+
 export * from './lib/spinner.module';
